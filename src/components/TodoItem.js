f63cf3882f6c4bb3d8700e2db13613018be85693
Francis John <pelaezfrancisjohn@gmail.com>
2023-05-05 20:01:05 +0800

    renamed the props from editTodo to handleEdit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const TodoItem = ({task, markComplete, deleteTodo, editTodo}) => {
+export const TodoItem = ({task, markComplete, deleteTodo, handleEdit}) => {
   return (
       <div className='todo-item-wrapper'>
-        <div onClick={markComplete} className={task.completed ? "completed" : ""}>{task.task}</div>
+        <p onClick={markComplete} className={task.completed ? "completed" : ""}>{task.task}</p>
         <div className='actions-container'>
-          <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={editTodo}/>
+          <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={handleEdit}/>
           <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={deleteTodo}/>
         </div>
       </div>
