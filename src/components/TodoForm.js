dd30c61719a7f1f31d9f4af4416c0076b0dabe0e
Francis John <pelaezfrancisjohn@gmail.com>
2023-05-05 19:59:29 +0800

    added useEffect hook and fixed the update functionality

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,24 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
-export const TodoForm = ({addTodo}) => {
+export const TodoForm = ({addTodo, onUpdate, editingTodoId, todos}) => {
   const [value, setValue] = useState('');
-  
 
+  useEffect(() => {
+    // If editingTodoId changes, update the form input value
+    if (editingTodoId !== null) {
+      const editedTodo = todos.find(todo => todo.id === editingTodoId);
+      if (editedTodo) {
+        setValue(editedTodo.task);
+      }
+    } else {
+      setValue('');
+    }
+  }, [editingTodoId, todos]);
+  
+  const onInputchange = (event) => {
+    setValue(event.target.value);
+  }
   const handleSubmit = e => {
     e.preventDefault();
-    addTodo(value);
-    setValue('');
+    if (editingTodoId !== null) {
+      onUpdate(editingTodoId, value);
+    } else {
+      addTodo(value);
+    }
+    setValue("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div className='form-wrapper'>
-        <input type='text' placeholder='What needs to be done?'
+        <input type='text' placeholder={editingTodoId !== null ? 'Edit your task' : 'What needs to be done?'}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={onInputchange}
+        required
         />
-        <button type='submit' className='button-6'>Add Task</button>
+        <button type='submit' className='button-6'>{editingTodoId !== null ? 'Update' : 'Add'}</button>
       </div>
     </form>
   )
