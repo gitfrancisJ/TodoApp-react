060f35c93c15de13442438ca1e6b27b6cf8835c9
Francis John <pelaezfrancisjohn@gmail.com>
2023-05-05 19:58:00 +0800

    added useEffect hooks and added update functionality

--- src/components/TodoAppWrapper.js
+++ src/components/TodoAppWrapper.js
@@ -1,15 +1,15 @@
 import '../index.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { TodoForm } from './TodoForm';
 import { TodoItem } from './TodoItem';
-import { EditTodo } from './EditTodo';
 
 export const TodoAppWrapper = () => {
   const [todos,setTodos] = useState([]);
+  const [editingTodoId, setEditingTodoId] = useState(null);
 
   const addTodo = todo => {
-    setTodos([{id: todos.length, task: todo, completed: false, isEditing: false}, ...todos]);
-    // console.log(...todos);
+    setTodos([ ...todos, {id: todos.length, task: todo, completed: false}]);
+    
   }
 
   const markComplete = (id) => {
@@ -21,28 +21,34 @@ export const TodoAppWrapper = () => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
-  const editTodo = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? {
-      ...todo, isEditing: !todo.isEditing} : todo))
+  const handleEdit = (id) => {
+    setEditingTodoId(id);
+  };
+
+  const handleUpdate = (id, task) => {
+      setTodos(
+        todos.map(todo => (todo.id === id ? { ...todo, task } : todo))
+      );
+    setEditingTodoId(null);
   }
+
+  useEffect(() => {
+  }, [todos, editingTodoId])
   return (
     <div className='todo-app-wrapper'>
       <p className='title'>To-do App Reactjs</p>
 
       <div className='todo-form-container'>
-        <TodoForm addTodo={addTodo}/>
+        <TodoForm addTodo={addTodo}  onUpdate={handleUpdate} editingTodoId={editingTodoId} todos={todos}/>
         <div className='todos-container'>
-       
           {
             todos.map((todo) => (
-              todo.isEditing ? <EditTodo key={todo.id}/>
-              :
-              <TodoItem task={todo} key={todo.id} markComplete={() => markComplete(todo.id)} deleteTodo={() => deleteTodo(todo.id)} editTodo={editTodo(todo.id)}/>
-              
+              <TodoItem task={todo} key={todo.id} markComplete={() => markComplete(todo.id)} deleteTodo={() => deleteTodo(todo.id)} handleEdit={() => handleEdit(todo.id)}/>
             ))
           }
         </div>
       </div>
+      <p className='powered-by'>Powered by: Reactjs</p>
     </div>
   )
 }
