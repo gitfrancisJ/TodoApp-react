86382bb0705588a7167ec75a37aff9b07038f6d0
Francis John <pelaezfrancisjohn@gmail.com>
2023-05-08 20:41:13 +0800

    added an undo icon to mark the todo as not completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare, faTrash, faCheck, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
 export const TodoItem = ({task, markComplete, deleteTodo, handleEdit}) => {
   return (
       <div className='todo-item-wrapper'>
         <p className={task.completed ? "completed" : ""}>{task.task}</p>
         <div className='actions-container'>
-          <FontAwesomeIcon className="check-icon" icon={faCheck} onClick={markComplete} />
+          { task.completed ? <FontAwesomeIcon className="undo-icon" icon={faRotateLeft} onClick={markComplete}/> :
+          <FontAwesomeIcon className="check-icon" icon={faCheck} onClick={markComplete} />}
           <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={handleEdit} />
           <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={deleteTodo} />
         </div>
