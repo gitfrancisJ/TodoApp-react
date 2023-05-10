0429d873455e7c7d82a7fba3b353ec9df7ca6f58
Francis John <pelaezfrancisjohn@gmail.com>
2023-05-10 17:46:56 +0800

    added functions for todo-app

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -5,10 +5,28 @@ export const TodoContext = createContext([]);
 export const DataProvider = (props) => {
 
   const [todos,setTodos] = useState([]);
+  const [editingTodoId, setEditingTodoId] = useState(null);
+
+  const addTodo = (todo) => {
+    setTodos([{id: todos.length, task: todo, completed: false}, ...todos,]);
+  }
+  const markComplete = (id) => {
+    setTodos(todos.map(todo => todo.id === id ? {
+      ...todo, completed: !todo.completed} : todo))
+  };
+  
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  
+  const handleEdit = (id) => {
+    setEditingTodoId(id);
+  };
+
 
 
   return (
-    <TodoContext.Provider value={[todos,setTodos]}>
+    <TodoContext.Provider value={[todos,setTodos,addTodo, markComplete, deleteTodo, editingTodoId, setEditingTodoId, handleEdit]}>
       {props.children}
     </TodoContext.Provider>
   )
